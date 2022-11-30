44f7433924fe21561f140331120734ef4734f490
ryo121w <ryoutaoru9121@icloud.com>
2022-11-30 11:43:13 +0900

    dev_basis03

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,18 +6,23 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     
+    
+    
+    
+    
     <body　class="antialiased">
+        
         <h1>Blog Name</h1>
+        <a href="/posts/create">create</a>
         <div class="posts">
+            
             @foreach($posts as $post)
             <div class="post">
-             <a href="/posts/{{ $post-.id }}"><h2 class='title'>{{ $post->title }}</h2></a>
+             <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
                 <p class="body">{{ $post->body }}</p>
             </div>
             @endforeach
         </div>
-        <div class='paginate'>
-            {{ $posts->links()}}
-        </div>
+       
     </body>
 </html>
