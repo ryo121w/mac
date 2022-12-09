7917b42b370db43d083f3350896fbbd61b66d188
ryo121w <ryoutaoru9121@icloud.com>
2022-12-09 21:56:05 +0900

    dev_basis03

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,21 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+
 
 class Post extends Model
 {
     use HasFactory;
-    
-    
-    protected $fillable = [
-        'title',
-        'body'
-        ];
-    
-    public function getPaginateByLimit(int $limit_count = 5)
-    {
-    return $this->orderby('updated_at', 'DESC')->paginate($limit_count);
-    }
-    
+  
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,23 +8,6 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>Blog Name</h1>
-      <form action="/posts" method="POST">
-          @csrf
-          <div class="title">
-              <h2>Title</h2>
-              <input type="text" name=post[title] placeholder="タイトル">
-              
-          </div>
-          
-          <div class="body">
-              <h2>Body</h2>
-              <textarea name="post[body]" placeholder="今日も１日お疲れ様でした"></textarea>
-          </div>
-          <input type="submit" value="store">
-      </form>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
+      
     </body>
 </html>
